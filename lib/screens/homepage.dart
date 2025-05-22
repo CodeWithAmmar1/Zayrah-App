b47de66906dd5bc63f112ff409aab8654c5be51c
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-05-22 16:50:16 +0500

    done

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -12,8 +12,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int? tappedIndex;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
