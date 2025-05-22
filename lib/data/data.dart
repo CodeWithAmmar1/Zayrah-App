4d12a36f7cccac3d04f5dd48e326bd7aa1422c7d
CodeWithAmmar1 <m.aliammar.naseer@gmail.com>
2025-05-22 16:40:17 +0500

    updatinging ui

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -4,36 +4,36 @@ final List<Product> products = [
   Product(
     name: "Plain Shirt",
     imageUrl: "https://media.istockphoto.com/id/1726263781/vector/blank-black-t-shirt-template.jpg?s=612x612&w=0&k=20&c=D99VMv8nYh1tmSZO61Do3OYbUldC4JMQt7OiyyTaLG8=",
-    price: 90.99,
+    price: 40.99,
   ),
   Product(
-    name: "Men Watch",
+    name: "Mens Watch",
     imageUrl: "https://media.istockphoto.com/id/1180244659/photo/luxury-watch-isolated-on-white-background-with-clipping-path-for-artwork-or-design-black.jpg?s=612x612&w=0&k=20&c=yeFNfkQmcVV9BTUlZO8vY_oLOQgDAt23LfCbF1e3fbI=",
-    price: 490.99,
+    price: 102.99,
   ),
   Product(
     name: "Yellow suitcase",
     imageUrl: "https://media.istockphoto.com/id/1400155112/photo/yellow-suitcase-flying-on-white-background.jpg?s=612x612&w=0&k=20&c=U5x1IIaOXIDZ-Q59mvDD4V-I8bWnFWZ7flh03Up3sZ4=",
-    price: 390.99,
+    price: 227.99,
   ),
   Product(
     name: "Sunglasses",
     imageUrl: "https://media.istockphoto.com/id/1152865943/photo/cat-eye-sunglasses-isolated-on-white.jpg?s=612x612&w=0&k=20&c=JJQY9vhZTjyz1ShYg1wcJUgovQVubNPrQJaCpkffgwI=",
-    price: 190.99,
+    price: 55.99,
   ),
    Product(
     name: "Nike Shoes",
     imageUrl: "https://media.istockphoto.com/id/471565617/photo/black-and-red-air-jordan-sneaker.jpg?s=612x612&w=0&k=20&c=rlJ1ImnpznBnRNfz45IDRTfFRnfqRuYAdvJA8dQPBXM=",
-    price: 490.99,
+    price: 125.99,
   ),
   Product(
-    name: "Nike Shoes",
-    imageUrl: "https://media.istockphoto.com/id/471565617/photo/black-and-red-air-jordan-sneaker.jpg?s=612x612&w=0&k=20&c=rlJ1ImnpznBnRNfz45IDRTfFRnfqRuYAdvJA8dQPBXM=",
-    price: 490.99,
+    name: "Mens Belt",
+    imageUrl: "https://media.istockphoto.com/id/523818848/photo/brown-leather-belt.jpg?s=612x612&w=0&k=20&c=DxKzRhgSVBl05Jtky4CrR9vf46twZRIZZhN5vXERPls=",
+    price: 11.31,
   ),
   Product(
-    name: "Nike Shoes",
-    imageUrl: "https://media.istockphoto.com/id/471565617/photo/black-and-red-air-jordan-sneaker.jpg?s=612x612&w=0&k=20&c=rlJ1ImnpznBnRNfz45IDRTfFRnfqRuYAdvJA8dQPBXM=",
-    price: 490.99,
+    name: "iPhone 11 Pro",
+    imageUrl: "https://media.istockphoto.com/id/1190447864/photo/apple-iphone-11-pro-gray-smartphone.jpg?s=612x612&w=0&k=20&c=zETLJeguLoTEFBNKPl1vjPY1lvPW1uM6GPpyiMSvsC0=",
+    price: 999.10,
   ),
 ];
